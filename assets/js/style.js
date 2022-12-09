476de20c06bcc2ec6d7552ed16c65058f2db4966
Kelvinsinferno <kelvinsinferno@gmail.com>
2022-12-08 18:20:48 -0700

    idk

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,32 +1,70 @@
-var APIKey = "6d7c9acad2b9d5b7c790098d562f8fd2"
-//or 272cb259234e49706c3aa35551daa022
+var APIKey = "6d7c9acad2b9d5b7c790098d562f8fd2";
+var search = [];
+var input = document.querySelector('#input');
 
+search.addEventListener('click', apiRequest);
+atlanta.addEventListener('click', apiRequest);
+austin.addEventListener('click', apiRequest);
+chicago.addEventListener('click', apiRequest);
+denver.addEventListener('click', apiRequest);
+newYork.addEventListener('click', apiRequest);
+orlando.addEventListener('click', apiRequest);
+sanFrancisco.addEventListener('click', apiRequest);
+seattle.addEventListener('click', apiRequest);
+
+//or 272cb259234e49706c3aa35551daa022
 //  direct geocoding Api
-fetch('http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={APIKey}', {
-    method: 'GET', //GET is the default.
-    credentials: 'same-origin', // include, *same-origin, omit
-    redirect: 'follow', // manual, *follow, error
-  })
+function location(location) {
+  fetch('http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={APIKey}',
+  )
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-    });
+      renderItems(cityString, data);
+    })
+    .catch(function (err) {
+      console.error(err);
+    })
 
-
-
-
-
-
-
+}
 
 // weather api api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={6d7c9acad2b9d5b7c790098d562f8fd2}
-fetch(`api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey}`)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+function weather(search) {
+  fetch(`api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (!data[0]) {
+        alert('We have no data for this location')
+      }
+    }).catch(function (err) {
+      console.error(err)
+    });
+}
+
+
+
+
+
+
+function renderWeather(search) {
+  if (search.indexOf(search) !== -1) {
+    return;
+  }
+  search.push(search);
+  localStorage.setItem('search', JSON.stringify(search));
+  renderSearchWeather();
+}
+
+function getSearchWeather() {
+  var search = JSON.parse(localStorage.getItem('search'));
+  if (!search) {
+    search = [];
+  }
+  renderSearchWeather();
+}
+
+
 
